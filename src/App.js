b9165a2c6cc4efc2247545fd09c1031d549f417c
Gaston82 <cabezaartaza@hotmail.com>
2022-04-12 01:55:34 +0200

    add getTrendingGifs service

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Gift from "./components/gift";
 import SearchBox from "./components/searchBox";
-import { getGifs } from "./services/getGifs";
+import { getGifs, getTrendingGifs } from "./services/getGifs";
 
 function App() {
   const [query, setQuery] = useState("maradona");
   const [listOfGifs, setListOfGifs] = useState([]);
+  const [trendingGifs, setTrendingGifs] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchGifs = async () => {
+  //     const result = await getGifs(query);
+  //     setListOfGifs(result);
+  //   };
+
+  //   fetchGifs();
+  // }, [query]);
 
   useEffect(() => {
-    const fetchGifs = async () => {
-      const result = await getGifs(query);
-      setListOfGifs(result);
+    const fetchTrendingGifs = async () => {
+      const result = await getTrendingGifs();
+      setTrendingGifs(result);
     };
-
-    fetchGifs();
-  }, [query]);
+    fetchTrendingGifs();
+  }, []);
 
   return (
+    // <div className="App">
+    //   <SearchBox setQuery={setQuery} />
+    //   {listOfGifs.map((gif) => (
+    //     <Gift key={gif.id} name={gif.name} image={gif.image} />
+    //   ))}
+    // </div>
     <div className="App">
       <SearchBox setQuery={setQuery} />
-      {listOfGifs.map((gif) => (
-        <article key={gif.id}>
-          <p style={{ color: "#5FD6F7" }}>{gif.name}</p>
-          <img src={gif.image} alt={gif.name} />
-        </article>
+      {trendingGifs.map((gif) => (
+        <Gift key={gif.id} name={gif.name} image={gif.image} />
       ))}
     </div>
   );
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -18,3 +18,37 @@ export async function getGifs(search) {
   }
 }
 //id,title,image,downsized_medium.url
+
+export function getData(search) {
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=10&offset=0&rating=g&lang=en`;
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => console.table("getdata", data.data));
+}
+
+// export function getTrendingGifs() {
+//   // https://api.giphy.com/v1/gifs/trending?api_key=8vG6z2FesxcljPGMEGNRT0hP79exFH21&limit=25&rating=g
+//   const url = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=20&rating=g`;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.data));
+// }
+
+export async function getTrendingGifs() {
+  try {
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=20&rating=g`;
+    const response = await fetch(url);
+    const { data } = await response.json();
+    const trendingGifs = data.map((gif) => {
+      return {
+        id: gif.id,
+        name: gif.title,
+        image: gif.images.downsized_medium.url,
+      };
+    });
+    return trendingGifs;
+  } catch (error) {
+    console.log(error.message);
+  }
+  // console.log(data.data);
+}
